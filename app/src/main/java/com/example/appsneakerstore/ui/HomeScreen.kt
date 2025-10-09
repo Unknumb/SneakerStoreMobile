78782efbfe6e98210b89b2deb302e2a860db83c7
jutoledod <ju.toledod@duocuc.cl>
2025-10-09 13:02:18 -0300

    Modificacion de HomeScreen

--- app/src/main/java/com/example/appsneakerstore/ui/HomeScreen.kt
+++ app/src/main/java/com/example/appsneakerstore/ui/HomeScreen.kt
@@ -1,47 +1,51 @@
 package com.example.appsneakerstore.ui
-import androidx.compose.foundation. Image
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
-import androidx.compose.material3. ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview. Preview import com.example.appsneakerstore.R
-import androidx.compose.material3. Scaffold
-import androidx.compose.material3.TopAppBar
+import com.example.appsneakerstore.R
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(){
-    Scaffold (
+fun HomeScreen() {
+    Scaffold(
         topBar = {
             TopAppBar(title = { Text("Mi App Kotlin") })
         }
-    ){ innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
-                    .padding(16.dp),
-                 Arrangement.spacedBy(20.dp)
-            ) {
-                Text(text = "Bienvenido")
-                Button(onClick = { /* acción futura */ }) {
-                    Text("Presióname")
-                }
-                Image(
-                    painter = painterResource (id = R.drawable.logo),
-                    contentDescription = "Logo App",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(150.dp),
-                    contentScale = ContentScale.Fit
-                )
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(text = "Bienvenido")
+            Button(onClick = { /* acción futura */ }) {
+                Text("Presióname")
             }
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
         }
+    }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
